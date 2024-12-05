aaef767e11825c4e7c55e28aeae65adf92a00dca
guswhdleee <gksrmrtn123@naver.com>
2024-12-05 16:17:15 +0900

    Merge branch 'main' of https://github.com/uzoolove/febc11-react

---
+++ workspace/app/todolist/06-react-query/src/pages/TodoEdit.jsx
@@ -0,0 +1,106 @@
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import {
+  Link,
+  useNavigate,
+  useOutletContext,
+  useParams,
+} from "react-router-dom";
+
+function TodoEdit() {
+  // Outlet 컴포넌트의 context 속성에 전달되는 값 추출
+  const { item } = useOutletContext();
+  const { _id } = useParams();
+  console.log("아이디 출력", _id);
+
+  // 프로그래밍 방식으로 페이지 이동에 사용
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: item.title,
+      content: item.content,
+      done: item.done,
+    },
+  });
+
+  // axios 인스턴스
+  const axios = useAxiosInstance();
+  const QueryClient = useQueryClient();
+
+  // React Query 수정 작업
+  const editItem = useMutation({
+    mutationFn: (item) => axios.patch(`/todolist/${_id}`, item),
+    onSuccess: () => {
+      alert("할일이 수정 되었습니다.");
+      // 지정한 키의 쿼리를 무효화
+      QueryClient.invalidateQueries(["todolist"]);
+      // 할일 상세보기로 이동
+      navigate(-1);
+    },
+    onError: (err) => {
+      console.error("서버에서 에러 응답");
+      alert(err?.message || "할일 수정에 실패했습니다.");
+    },
+  });
+
+  //   // 수정 작업
+  //  const onSubmit = async (formData) => {
+  //     try{
+  //       // TODO: API 서버에 수정 요청
+  //       await axios.patch(`/todolist/${ item._id }`, formData);
+  //       alert('할일이 수정 되었습니다.');
+
+  //       // 할일 상세보기로 이동
+  //       navigate(-1);
+  //       refetch();
+  //     }catch(err){
+  //       console.error(err);
+  //       alert('할일 수정에 실패했습니다.');
+  //     }
+  //   };
+
+  return (
+    <>
+      <h2>할일 수정</h2>
+      <div className="todo">
+        <form onSubmit={handleSubmit(editItem.mutate)}>
+          <label htmlFor="title">제목 :</label>
+          <input
+            type="text"
+            id="title"
+            autoFocus
+            {...register("title", {
+              required: "제목을 입력하세요.",
+            })}
+          />
+          <div className="input-error">{errors.title?.message}</div>
+          <br />
+          <label htmlFor="content">내용 :</label>
+          <textarea
+            id="content"
+            cols="23"
+            rows="5"
+            {...register("content", {
+              required: "내용을 입력하세요.",
+            })}
+          />
+          <div className="input-error">{errors.content?.message}</div>
+          <br />
+          <label htmlFor="done">완료 :</label>
+          <input type="checkbox" id="done" {...register("done")} />
+          <br />
+          <button type="submit">수정</button>
+          <Link to={`/list/${item._id}`}>취소</Link>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default TodoEdit;
--- workspace/ch09-ajax/02-nike-axios/src/App.jsx
+++ workspace/ch09-ajax/02-nike-axios/src/App.jsx
@@ -1,37 +1,35 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Product from "./Product";
 import Shipping from "./Shipping";
+import { DotLoader } from 'react-spinners';
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { Slide, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  console.log('App 렌더링.');
-
-  // const data = useMemo(() => ({
-  //   _id: 2,
-  //   price: 125000,
-  //   shippingFees: 3000,
-  //   name: '나이키 잼',
-  //   quantity: 35,
-  //   buyQuantity: 10,
-  //   mainImage: "/files/00-nike/NIKE_JAM_01.jpg",
-  //   content: '나이키가 세계적인 무대에 오르는 브레이크 댄서를 위해 제작한 첫 신발인 잼과 함께 몸과 마음, 정신을 하나로 만들어 보세요. 신발의 모든 디테일을 꼼꼼히 제작했기 때문에 자신 있게 사이퍼에 도전할 수 있습니다. 유연하고 내구성이 뛰어난 갑피가 몸을 따라 움직이며, 중창의 텍스처 처리된 핸드 그립 덕분에 공중에서 신발을 쉽게 잡을 수 있습니다. 그리고 위아래가 뒤집힌 로고를 배치해 프리즈 동작을 할 때 로고가 똑바로 보이는 재미를 더했죠.'
-  // }), []);
-
   const [data, setData] = useState(); // 1(마운트)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const axios = useAxiosInstance();
 
   const fetchData = async (_id) => {
-    const res = await fetch(`https://11.fesp.shop/products/${_id}`, {
-      headers: {
-        'client-id': '00-nike'
-      }
-    });
-    console.log('res', res);
-    const jsonData = await res.json();
-    console.log('jsonData', jsonData);
-    setData(jsonData.item); // 4번(마운트 후)
+    setIsLoading(true);
+    try{
+      // const res = await axios.get(`/products/${_id}`, { params: { delay: 1000 } });
+      const res = await axios.get(`/productssdfsdfds/${_id}`);
+      setData(res.data.item); // 4번(마운트 후)
+      setError(null);
+    }catch(err){ // network 에러, 4xx, 5xx 응답일 경우
+      setError(err);
+      setData(null);
+    }finally{
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-    fetchData(1); // 3번(마운트 후)
+    fetchData(7); // 3번(마운트 후)
   }, []); // 마운트 된 이후에 최초 한번만 실행
 
   const basicShippingFees = 3000;
@@ -53,8 +51,9 @@ function App() {
   // return <h1></h1> // 2번(마운트)
   return (
     <>
-      <h1>01 Nike 상품 상세 조회</h1>
-
+      <h1>02 Nike 상품 상세 조회 - Axios</h1>
+      { isLoading && <DotLoader /> }
+      {/* { error && <p>{ error.message }</p> } */}
       { data && (
         <div>
           <Product product={ data } />
@@ -69,6 +68,19 @@ function App() {
           <Shipping fees={ shippingFees } handlePayment={ handlePayment } />
         </div>
       ) }
+      <ToastContainer 
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
     </>
   );
 }
