b5ce4c837e7bfd5909d5ed4e0be9fb9a0204d432
guswhdleee <gksrmrtn123@naver.com>
2024-12-12 10:24:54 +0900

    다시 푸쉬

--- workspace/ch11-skeleton/lion-board/src/App.jsx
+++ workspace/ch11-skeleton/lion-board/src/App.jsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
-import { useTheme } from "styled-components";
 import useThemeStore from "@zustand/themeStore";
 
 function App() {
