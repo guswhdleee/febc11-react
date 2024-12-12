3b498ace8d7424cb887df9bdec6df5e4c0054d05
guswhdleee <gksrmrtn123@naver.com>
2024-12-12 11:28:28 +0900

    ㄴㅇㄹㄴㅁㅇㄹ

--- workspace/ch11-skeleton/lion-board/src/pages/index.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/index.jsx
@@ -3,26 +3,6 @@ import { Link } from "react-router-dom";
 export default function MainPage() {
   return (
     <main className="container mx-auto mt-10 p-4 transition-color">
-      <meta
-        name="description"
-        content="다양한 주제의 커뮤니티와 활발한 소통을 위한 플랫폼입니다. 관심사에 따라 참여하고, 의견을 나누세요."
-      />
-      <meta
-        name="keywords"
-        content="커뮤니티, 소통, 포럼, 관심사, 온라인 모임, 커뮤니티 서비스"
-      />
-      <meta name="author" content="Front End Boot Camp" />
-
-      <meta property="og:title" content="멋사컴에 오신걸 환영합니다." />
-      <meta
-        property="og:description"
-        content="유용한 정보를 나누고 공유하세요."
-      />
-      <meta property="og:image" content="/images/febc.png" />
-      <meta property="og:url" content="https://board.fesp.shop" />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="멋사컴" />
-
       <section className="text-center">
         <h1 className="text-4xl font-bold mb-4">
           멋사컴에 오신 것을 환영합니다!
