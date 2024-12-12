0c8c7d800a8911a2f53633773dd3589f284f8d5e
guswhdleee <gksrmrtn123@naver.com>
2024-12-12 10:33:12 +0900

    sadfsadf

--- workspace/ch11-skeleton/lion-board/src/pages/board/CommentList.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/CommentList.jsx
@@ -1,22 +1,22 @@
-import CommentListItem from "@pages/board/CommentListItem";
+import CommentListItem from "@pages/board/CommentListitem";
 import CommentNew from "@pages/board/CommentNew";
 import PropTypes from "prop-types";
 
 CommentList.propTypes = {
   data: PropTypes.array,
 };
-export default function CommentList({ data=[] }) {
-
-  const list = data.map(item => <CommentListItem key={item._id} item={ item } />);
+export default function CommentList({ data = [] }) {
+  const list = data.map((item) => (
+    <CommentListItem key={item._id} item={item} />
+  ));
 
   return (
     <section className="mb-8">
-      <h4 className="mt-8 mb-4 ml-2">댓글 { list.length }개</h4>
+      <h4 className="mt-8 mb-4 ml-2">댓글 {list.length}개</h4>
 
-      { list }
+      {list}
 
       <CommentNew />
-
     </section>
   );
 }
